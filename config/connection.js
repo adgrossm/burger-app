a8c51201994970524958cc1ebfe042128b8387d6
Adam Grossman <adgrossm@gmail.com>
2020-09-10 21:04:50 -0400

    updates to connection.js for JAWSBD

--- config/connection.js
+++ config/connection.js
@@ -6,14 +6,19 @@ var mysql = require("mysql");
 
 
 // MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "rootroot",
     database: "burgers_db"
   });
-
+}
 //   var PORT = process.env.PORT || 8080;
 //  Make connection
   connection.connect(function(err) {
